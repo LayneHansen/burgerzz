338b764a315b1c4bc65be022ccf12aca214b4865
LayneHansen <layne.d.hansen@gmail.com>
2020-10-20 20:55:44 -0600

    updated create burger function

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -5,22 +5,38 @@
 $(function () {
   $("#submit").on("submit", function(event) {
     event.preventDefault();
+    var id = $(this).data("id");
+    var newBurger = $(this).data("burger-text");
 
-    var newBurger = {
+    var newBurgerMade = {
       name: $("burger-text").val().trim(),
     };
-
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    })
-    .then(function () {
-      console.log(newBurger);
-      console.log("Created new burger");
-      location.reload();
+    
+    $.ajax("/api/cats/" + id {
+      type: "PUT",
+      data: newBurgerMade
+    }).then(
+      function() {
+        console.log("Created new burger", newBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+      );
     });
-  });
+    
+  //   $.ajax("/api/burgers", {
+  //     type: "POST",
+  //     data: newBurger
+  //   })
+  //   .then(function () {
+  //     console.log(newBurger);
+  //     console.log("Created new burger");
+  //     location.reload();
+  //   });
+  // });
 });
 
+// 
+
 // Crush a burger
-  // moves to Crushed column
+// moves to Crushed column
